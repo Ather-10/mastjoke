d4271d9ca2e0072aa56c0c62d2e5bd44f6f097b5
Ather-10 <chatherusman05@gmail.com>
2025-09-16 00:19:51 +0500

    image setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Joke from './components/Joke'
+import joke from './assets/joke.jpg'
 
 function App() {
   return (
     <>
     <div className="bg-indigo-500 min-h-screen flex flex-col items-center  justify-center">
-      <img src="..assets/joke.jpg" className='w-60 h-30 md:w-80 md:h-40 rounded-lg' alt="" />
+      <img src={joke} className='w-60 h-30 md:w-80 md:h-40 rounded-lg' alt="" />
    <Joke/>
 </div>
 
